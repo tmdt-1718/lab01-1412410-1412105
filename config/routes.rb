8a7e4ac41fcd34d6ba40e61141c618eb3f8cc09b
Si Phu Pham <psphu96@gmail.com>
2017-10-01 13:56:00 +0700

    Adding team information in about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/articles', to:'articles#index'
+  get '/articles', to:'articles#index'
   get '/', to: 'home#index'
   resources :articles, only: [:index, :show]
   resources :album, only: [:index]
