bbc0a785f6b75348165b4809cfb05b547abe74b1
Unknown <1412105@student.hcmus.edu.vn>
2017-10-04 11:21:54 +0700

    Database beta upload 2

--- db/migrate/20170930045830_create_tags.rb
+++ db/migrate/20170930045830_create_tags.rb
@@ -1,9 +1,9 @@
 class CreateTags < ActiveRecord::Migration[5.1]
   def change
-    create_table :tags, id: false do |t|
-      t.integer :tag_id, id: true, primary_key: true
+    create_table :tags do |t|
       t.string :tag_name, unique: true
       t.timestamps
     end
+    rename_column :tags, :id, :tag_id
   end
 end
