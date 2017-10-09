8a22ff54447628dc51014bd7177a312ff614f6da
1412105 <1412105@student.hcmus.edu.vn>
2017-10-09 23:18:28 +0700

    Fix error to migrate to deploy

--- db/migrate/20171009161737_change_object02.rb
+++ db/migrate/20171009161737_change_object02.rb
@@ -1,4 +1,4 @@
-class ChangObjectBcommentAndPcomment < ActiveRecord::Migration[5.1]
+class ChangeObject02 < ActiveRecord::Migration[5.1]
   def change
     drop_column :albums, :tag_id
     drop_column :blogs, :tag_id
