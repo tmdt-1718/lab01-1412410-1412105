28a62fcf4da71b2077b199226ecafd93add60b24
1412105 <1412105@student.hcmus.edu.vn>
2017-10-09 22:57:45 +0700

    Fix error to migrate to deploy

--- db/migrate/20171009153528_chang_object_bcomment_and_pcomment.rb
+++ db/migrate/20171009153528_chang_object_bcomment_and_pcomment.rb
@@ -1,12 +1,7 @@
 class ChangObjectBcommentAndPcomment < ActiveRecord::Migration[5.1]
   def change
-    rename_column :bcomments, :destination, :blog_id
-    rename_column :bcomments, :author, :user_id
-    rename_column :pcomments, :destination, :photo_id
-    rename_column :pcomments, :author, :user_id
-    drop_column :albums, :tag
-    rename_column :photos, :album, :album_id
-    drop_column :blogs, :tag
+    drop_column :albums, :tag_id
+    drop_column :blogs, :tag_id
     drop_column :users, :cover
   end
 end
