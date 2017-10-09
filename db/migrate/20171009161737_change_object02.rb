f97ff1c7fc71ad7c9f9a321f64c612704d480ac7
1412105 <1412105@student.hcmus.edu.vn>
2017-10-09 23:21:58 +0700

    Fix error to migrate to deploy

--- db/migrate/20171009161737_change_object02.rb
+++ db/migrate/20171009161737_change_object02.rb
@@ -1,8 +1,8 @@
 class ChangeObject02 < ActiveRecord::Migration[5.1]
   def change
-    drop_column :albums, :tag_id
-    drop_column :blogs, :tag_id
-    drop_column :users, :cover
-    drop_column :albums, :cover
+    remove_column :albums, :tag_id
+    remove_column :blogs, :tag_id
+    remove_column :users, :cover
+    remove_column :albums, :cover
   end
 end
