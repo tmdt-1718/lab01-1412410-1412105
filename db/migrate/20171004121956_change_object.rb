3f957602e9dbd058d361c6c6d79374a19b02bdb4
1412105 <1412105@student.hcmus.edu.vn>
2017-10-09 21:43:40 +0700

    Fix error to migrate to deploy

--- db/migrate/20171004121956_change_object.rb
+++ db/migrate/20171004121956_change_object.rb
@@ -5,12 +5,12 @@ class ChangeObject < ActiveRecord::Migration[5.1]
   	rename_column :blogs, :author, :user_id
   	rename_column :albums, :author, :user_id
     # rename_column :albums, :cover, :photo_id
-    # rename_column :albums, :tag, :tag_id
-    # rename_column :bcomments, :destination, :blog_id
-    # rename_column :bcomments, :author, :user_id
-    # rename_column :blogs, :tag, :tag_id
-    # rename_column :pcomments, :destination, :blog_id
-    # rename_column :pcomments, :author, :user_id
-  	# rename_column :photos, :album, :album_id
+    rename_column :albums, :tag, :tag_id
+    rename_column :bcomments, :destination, :blog_id
+    rename_column :bcomments, :author, :user_id
+    rename_column :blogs, :tag, :tag_id
+    rename_column :pcomments, :destination, :blog_id
+    rename_column :pcomments, :author, :user_id
+  	rename_column :photos, :album, :album_id
   end
 end
